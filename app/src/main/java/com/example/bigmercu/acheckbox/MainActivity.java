b059c8d9a0cb5602e15417d73181f3eaa25a7664
bigmercu <bigmercu@gmail.com>
2016-08-13 09:27:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bigmercu/acheckbox/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.bigmercu.acheckbox;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.checkbox.BBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    BBox b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        b = (BBox) findViewById(R.id.BBox2);
+
+        b.setOnCheckedChangeListener(new BBox.OnCheckedChangeListener() {
+            @Override
+            public void onChange(boolean checked) {
+                Toast.makeText(getApplicationContext(),String.valueOf(checked),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ checkbox/src/main/java/com/example/checkbox/BBox.java
@@ -0,0 +1,364 @@
+package com.example.checkbox;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Checkable;
+
+/**
+ * Created by bigmercu on 16/7/23.
+ * Email:bigmercu@gmail.com
+ */
+
+public class BBox extends View implements Checkable{
+
+    private int hSize = dp2px(15);
+    private int wSize = dp2px(15);
+    private int textSize = dp2px(14);
+    private int wStart = dp2px(2);
+    private int hStart = dp2px(2);
+    private int Duration = 300;
+    private int strokeWidth = dp2px(2);
+    private int colorBefore = Color.BLACK;
+    private int colorAfter = Color.RED;
+    private String boxText = "CheckBox";
+    private Paint mPaint;
+    private Paint mPaintAfter;
+    private Paint mPaintText;
+    private Path mPath;
+    private Path mDst;
+    private PathMeasure pathMeasure;
+    private float pathLenth;
+    private int AnimationValue = 255;
+    private int AnimationValue1;
+    private float AnimationValue2;
+    private float AnimationValue3;
+    private float AnimationValue4;
+    private ValueAnimator valueAnimator;
+    private ValueAnimator valueAnimator1;
+    private ValueAnimator valueAnimator2;
+    private ValueAnimator valueAnimator3;
+    private ValueAnimator valueAnimator4;
+    private float cAnimationValue = 0;
+    private float cAnimationValue1 = 0;
+    private float cAnimationValue2 = 255;
+    private ValueAnimator cValueAnimator;
+    private ValueAnimator cValueAnimator1;
+    private ValueAnimator cValueAnimator2;
+    private boolean checked = false;
+    private boolean isHook = true;
+    private int mScreenWidth,mSreenHeight;
+    private OnCheckedChangeListener mOnCheckedChangeListener;
+
+    public BBox(Context context) {
+        super(context);
+    }
+
+    public BBox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint();
+
+        if(attrs != null){
+            TypedArray array = getContext().obtainStyledAttributes(attrs,R.styleable.BBox);
+            colorAfter = array.getColor(R.styleable.BBox_color_after,Color.RED);
+            colorBefore = array.getColor(R.styleable.BBox_color_before,Color.BLACK);
+            boxText = array.getString(R.styleable.BBox_check_text);
+            isHook = array.getInt(R.styleable.BBox_check_style, 1) == 1;
+            if(boxText == null){
+                boxText = "CheckBox";
+            }
+            array.recycle();
+        }else {
+            if(strokeWidth > 15) {
+                strokeWidth = dp2px(5);
+            }
+        }
+
+
+        mPaint.setColor(colorBefore);
+        mPaint.setStrokeWidth(strokeWidth);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeCap(Paint.Cap.SQUARE);
+
+        mPaintAfter = new Paint();
+        mPaintText = new Paint();
+
+        mPaintAfter.setColor(colorAfter);
+        mPaintAfter.setStrokeWidth(strokeWidth);
+        mPaintAfter.setStyle(Paint.Style.STROKE);
+        mPaintAfter.setAntiAlias(true);
+        mPaintAfter.setStrokeCap(Paint.Cap.SQUARE);
+
+        mPaintText.setColor(Color.BLACK);
+//        mPaintText.setStrokeWidth(strokeWidth);
+        mPaintText.setAntiAlias(true);
+        mPaintText.setStyle(Paint.Style.STROKE);
+        mPaintText.setTextSize(textSize);
+        mPaintText.setAntiAlias(true);
+
+        mPath = new Path();
+        pathMeasure = new PathMeasure();
+
+        mPath.addRect(wStart,hStart,wSize + wStart,hSize + hStart, Path.Direction.CW);
+        pathMeasure.setPath(mPath,true);
+        pathLenth = pathMeasure.getLength();
+
+        mDst = new Path();
+
+        if(isHook) {
+
+            valueAnimator = ValueAnimator.ofInt(255, 0);
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    AnimationValue = (int) animation.getAnimatedValue();
+                }
+            });
+
+            valueAnimator1 = ValueAnimator.ofInt(180, 225);
+            valueAnimator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    AnimationValue1 = (int) animation.getAnimatedValue();
+                }
+            });
+
+            valueAnimator2 = ValueAnimator.ofFloat(hSize + wSize, wSize * 2 / 5);
+            valueAnimator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    AnimationValue2 = (float) animation.getAnimatedValue();
+                }
+            });
+
+            valueAnimator3 = ValueAnimator.ofFloat(hSize + wSize, hSize + wSize * 2);
+            valueAnimator3.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    AnimationValue3 = (float) animation.getAnimatedValue();
+                }
+            });
+
+            valueAnimator4 = ValueAnimator.ofFloat((float) 0.0, (float) 0.207555);
+            valueAnimator4.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    AnimationValue4 = (float) animation.getAnimatedValue();
+                    postInvalidate();
+                }
+            });
+
+
+            valueAnimator.setDuration(Duration / 2);
+            valueAnimator1.setDuration(Duration);
+            valueAnimator2.setDuration(Duration);
+            valueAnimator3.setDuration(Duration);
+            valueAnimator4.setDuration(Duration);
+            valueAnimator.setInterpolator(new DecelerateInterpolator());
+            valueAnimator1.setInterpolator(new DecelerateInterpolator());
+            valueAnimator2.setInterpolator(new DecelerateInterpolator());
+            valueAnimator3.setInterpolator(new DecelerateInterpolator());
+            valueAnimator4.setInterpolator(new DecelerateInterpolator());
+        }else {
+            cValueAnimator = ValueAnimator.ofFloat(0,hSize/2);
+            cValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    cAnimationValue = (float) animation.getAnimatedValue();
+                }
+            });
+
+
+            cValueAnimator1 = ValueAnimator.ofFloat(0,wSize/2);
+            cValueAnimator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    cAnimationValue1 = (float) animation.getAnimatedValue();
+                }
+            });
+
+
+            cValueAnimator2 = ValueAnimator.ofFloat(255,0);
+            cValueAnimator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    cAnimationValue2 = (float) animation.getAnimatedValue();
+                    postInvalidate();
+                }
+            });
+            cValueAnimator.setDuration(Duration);
+            cValueAnimator1.setDuration(Duration);
+            cValueAnimator2.setDuration(Duration);
+            cValueAnimator.setInterpolator(new DecelerateInterpolator());
+            cValueAnimator1.setInterpolator(new DecelerateInterpolator());
+            cValueAnimator2.setInterpolator(new DecelerateInterpolator());
+        }
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setChecked(!checked);
+            }
+        });
+    }
+
+    public BBox(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int wMeasureSpec = MeasureSpec.makeMeasureSpec(dp2px(100),MeasureSpec.AT_MOST);
+        int hMeasureSpec = MeasureSpec.makeMeasureSpec(dp2px(30),MeasureSpec.EXACTLY);
+
+        setMeasuredDimension(wMeasureSpec,hMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    public void setChecked(boolean set){
+        if(checked == set){
+            return;
+        }else {
+            if(checked){
+                animationReverse();
+            }else {
+                animationStart();
+            }
+            checked = set;
+        }
+        if(mOnCheckedChangeListener != null){
+            mOnCheckedChangeListener.onChange(checked);
+        }
+        invalidate();
+    }
+
+    public boolean isChecked(){
+        return checked;
+    }
+
+    @Override
+    public void toggle() {
+        setChecked(!checked);
+    }
+
+
+    public void setText(String text){
+        boxText = text;
+        invalidate();
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mScreenWidth = w;
+        mSreenHeight = h;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mDst.reset();
+
+        if(isHook) {
+
+            mDst.lineTo(0,0);
+            canvas.translate(wStart, hStart);
+            canvas.drawText(boxText, wStart + wSize + 20, (hSize + textSize )/2, mPaintText);
+            //确保图形在正中
+            canvas.translate(-AnimationValue4 * wSize, -AnimationValue4 * hSize * 2);
+            /**
+             * 如果 startWithMoveTo 为 true, 则被截取出来到Path片段保持原状，如果 startWithMoveTo 为 false，
+             * 则会将截取出来的 Path 片段的起始点移动到 dst 的最后一个点，以保证 dst 的连续性。
+             * */
+
+            pathMeasure.getSegment(AnimationValue3, pathLenth, mDst, true);
+            pathMeasure.getSegment(0, wSize * 2 / 5, mDst, true);
+            pathMeasure.getSegment(wSize * 2 / 5, AnimationValue2, mDst, true);
+            mPaint.setAlpha(AnimationValue);
+            mPaintAfter.setAlpha(255 - AnimationValue);
+
+            canvas.rotate(AnimationValue1, wStart + wSize / 2, hStart + hSize / 2);
+            canvas.drawPath(mDst, mPaint);
+            canvas.drawPath(mDst, mPaintAfter);
+        }else {
+            float[] tan = new float[2];
+            float[] pos = new float[2];
+            canvas.translate(wStart, hStart);
+
+            mDst.moveTo(wStart,hStart);
+            canvas.drawText(boxText, wStart + wSize + 20, (hSize + textSize )/2, mPaintText);
+            mDst.lineTo(wStart + wSize/2,hStart + cAnimationValue);
+            mDst.lineTo(wStart + wSize,hStart);
+            mDst.lineTo(wStart + wSize - cAnimationValue1,hSize/2 + hStart);
+            mDst.lineTo(wStart + wSize,hStart + hSize);
+            mDst.lineTo(wStart + wSize/2,hStart + hSize - cAnimationValue);
+            mDst.lineTo(wStart,hStart + hSize);
+            mDst.lineTo(wStart + cAnimationValue,hStart + hSize/2);
+            mDst.lineTo(wStart,hStart);
+            mPaint.setAlpha((int) cAnimationValue2);
+            canvas.drawPath(mDst,mPaint);
+            mPaintAfter.setAlpha((int) (255-cAnimationValue2));
+            canvas.drawPath(mDst,mPaintAfter);
+        }
+
+    }
+
+
+
+    private void animationStart(){
+        if(isHook) {
+            valueAnimator.start();
+            valueAnimator1.start();
+            valueAnimator2.start();
+            valueAnimator3.start();
+            valueAnimator4.start();
+        }else {
+            cValueAnimator.start();
+            cValueAnimator1.start();
+            cValueAnimator2.start();
+        }
+    }
+
+    private void animationReverse(){
+        if(isHook) {
+            valueAnimator.reverse();
+            valueAnimator1.reverse();
+            valueAnimator2.reverse();
+            valueAnimator3.reverse();
+            valueAnimator4.reverse();
+        }else {
+            cValueAnimator.reverse();
+            cValueAnimator1.reverse();
+            cValueAnimator2.reverse();
+        }
+    }
+
+    public int dp2px(float value) {
+        final float scale = getContext().getResources().getDisplayMetrics().densityDpi;
+        return (int) (value * (scale / 160) + 0.5f);
+    }
+
+
+    public interface  OnCheckedChangeListener{
+        void onChange(boolean checked);
+    }
+
+    public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
+        this.mOnCheckedChangeListener = listener;
+    }
+}
